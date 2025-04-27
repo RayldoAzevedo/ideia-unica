5bd35a7de28079a86a387c2f2465893810ec7615
Rayldo Azevedo <rayldoazevedo@gmail.com>
2025-04-27 13:54:28 -0300

    fazendo graça

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,9 @@ import {useState} from 'react'
 function Home(){
     return <div>
         ESTOU COM FOME DONA CADÊ A COMIDA?
-        
         <div>BEIJOS VOCÊ É ESPECIAL PARA MIM</div>
+        <img src='https://www.instagram.com/p/C7R7_dNJkbY/' width="500"/>
     </div>
 }
-
-
-
 
 export default Home
